1475852791f89c67b9544ff40c2ebde3d97739e0
Chad Nguyen <chadn458@gmail.com>
2023-02-01 21:58:24 -0500

    first commit2nd

---
+++ wk2day3modulehw.py
@@ -0,0 +1,8 @@
+area = length * width
+width = int(input('Enter the width: '))
+length = int(input('Enter the length: '))
+
+
+pi = 3.14159265359
+Radius = int(input("Enter the radius of the circle: "))
+circumference = pi*radius*radius
